28229c07dddab74e1a7538d7d3f0ffc92dc2aa45
minh2k4 <nguyengiaminh1192004@gmail.com>
2025-01-15 12:35:39 +0700

    Add ThemMH.php file

---
+++ TrangChinh.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản Lý Điểm Học Phần</title>
+</head>
+<body>
+    <h1>Quản Lý Điểm Học Phần</h1>
+    <h1></h1>
+</body>
+</html>
